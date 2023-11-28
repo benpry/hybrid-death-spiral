0116fca3ca6e29fdc3eea38bfa10192706eeda6d
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2023-11-27 16:19:10 -0800

    updates to agents and report

--- agents.py
+++ agents.py
@@ -3,10 +3,12 @@ The code for hybrid meeting agents
 """
 import numpy as np
 
+
 class Agent:
     """
     A hybrid meeting agent that tries to match the majority of others.
     """
+
     def __init__(self, sp, sr, Bp, Br):
         self.sp = sp
         self.sr = sr
@@ -16,9 +18,11 @@ class Agent:
     def attends(self, Np_estimate, Nr_estimate, meeting_format):
         if meeting_format == "hybrid":
             # hybrid meeting: do we prefer in-person attendance?
-            if self.compute_utility(Np_estimate, Nr_estimate, "in-person") > \
-                self.compute_utility(Np_estimate, Nr_estimate, "remote") and \
-                self.compute_utility(Np_estimate, Nr_estimate, "in-person") > 0:
+            if (
+                self.compute_utility(Np_estimate, Nr_estimate, "in-person")
+                > self.compute_utility(Np_estimate, Nr_estimate, "remote")
+                and self.compute_utility(Np_estimate, Nr_estimate, "in-person") > 0
+            ):
                 return "in-person"
             # if not, would we rather attend remotely than not at all?
             elif self.compute_utility(Np_estimate, Nr_estimate, "remote") > 0:
@@ -27,10 +31,18 @@ class Agent:
                 return None
         elif meeting_format == "irl":
             # we can either attend in-person or not at all
-            return "in-person" if self.compute_utility(Np_estimate, Nr_estimate, "in-person") > 0 else None
+            return (
+                "in-person"
+                if self.compute_utility(Np_estimate, Nr_estimate, "in-person") > 0
+                else None
+            )
         elif meeting_format == "online":
             # we can either attend remotely or not at all
-            return "remote" if self.compute_utility(Np_estimate, Nr_estimate, "remote") > 0 else None
+            return (
+                "remote"
+                if self.compute_utility(Np_estimate, Nr_estimate, "remote") > 0
+                else None
+            )
 
     def compute_utility(self, Np, Nr, attendance_type):
         if attendance_type == "in-person":
@@ -40,20 +52,22 @@ class Agent:
         else:
             return 0
 
+
 class MeetingSequence:
     """
     A sequence of meetings
     """
+
     def __init__(
-            self,
-            n_agents,
-            meeting_format,
-            AgentType=Agent,
-            Bp_params=(-10, 5),
-            Br_params=(-2, 1),
-            sp_params=(1, 0.5),
-            sr_params=(0.2, 0.1),
-            hybrid_fixed_cost = 10
+        self,
+        n_agents,
+        meeting_format,
+        AgentType=Agent,
+        Bp_params=(-10, 5),
+        Br_params=(-2, 1),
+        sp_params=(1, 0.5),
+        sr_params=(0.2, 0.1),
+        hybrid_fixed_cost=10,
     ):
         self.agents = []
         for _ in range(n_agents):
@@ -63,7 +77,7 @@ class MeetingSequence:
             Br = np.random.normal(loc=Br_params[0], scale=Br_params[1])
             agent = AgentType(sp, sr, Bp, Br)
             self.agents.append(agent)
-        
+
         self.meeting_format = meeting_format
         self.n_agents = n_agents
         self.hybrid_fixed_cost = hybrid_fixed_cost
@@ -72,15 +86,20 @@ class MeetingSequence:
 
     def hold_meeting(self, meeting_format):
         """Hold a meeting and return the total utility"""
-        agent_attendances = [a.attends(self.in_person_estimate, self.remote_estimate, meeting_format) for a in self.agents]
+        agent_attendances = [
+            a.attends(self.in_person_estimate, self.remote_estimate, meeting_format)
+            for a in self.agents
+        ]
         n_in_person = sum([int(x == "in-person") for x in agent_attendances])
         n_remote = sum([int(x == "remote") for x in agent_attendances])
 
         sum_utility = 0
         for i, agent in enumerate(self.agents):
-            sum_utility += agent.compute_utility(n_in_person, n_remote, agent_attendances[i])
+            sum_utility += agent.compute_utility(
+                n_in_person, n_remote, agent_attendances[i]
+            )
         mean_utility = sum_utility / self.n_agents
-        
+
         # this iteration's actual number becomes the next iteration's estimate
         self.in_person_estimate = n_in_person
         self.remote_estimate = n_remote
@@ -101,7 +120,7 @@ class MeetingSequence:
             utilities.append(utility)
 
         return ns_in_person, ns_remote, utilities
-            
+
     def reset(self):
         if self.meeting_format == "online":
             self.remote_estimate = self.n_agents
@@ -115,7 +134,7 @@ class LogUtilityAgent(Agent):
     """
     A hybrid meeting agent that tries to match the majority of others.
     """
-    
+
     def compute_utility(self, Np, Nr, chose_in_person):
         if chose_in_person:
             return self.sp * np.log(Np)
--- utils.py
+++ utils.py
@@ -2,13 +2,22 @@ from agents import Agent, MeetingSequence
 import pandas as pd
 import numpy as np
 
+
 def run_simulations(params, n_sims=1000):
     rows = []
     for _ in range(n_sims):
         meeting_sequence = MeetingSequence(**params)
         ns_in_person, ns_remote, utilities = meeting_sequence.hold_meeting_sequence(10)
-        for i, (n_in_person, n_remote, utility) in enumerate(zip(ns_in_person, ns_remote, utilities)):
-            rows.append({"meeting_num": i, "n_in_person": n_in_person, "ns_remote": n_remote, "utility": utility})
+        for i, (n_in_person, n_remote, utility) in enumerate(
+            zip(ns_in_person, ns_remote, utilities)
+        ):
+            rows.append(
+                {
+                    "meeting_num": i,
+                    "n_in_person": n_in_person,
+                    "ns_remote": n_remote,
+                    "utility": utility,
+                }
+            )
 
     return pd.DataFrame(rows)
-
